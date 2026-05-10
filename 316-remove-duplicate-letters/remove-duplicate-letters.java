c21e906ffcae99143f3063a075f8f7827ceebe3c
manikanta <vtu26123@veltech.edu.in>
2026-05-10 23:11:59 +0530

    Time: 2 ms (89.36%) | Memory: 43.6 MB (81.60%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+
+    public String removeDuplicateLetters(String s) {
+
+        int[] count = new int[26];
+        boolean[] visited = new boolean[26];
+
+        for (char c : s.toCharArray())
+            count[c - 'a']++;
+
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            count[c - 'a']--;
+
+            if (visited[c - 'a'])
+                continue;
+
+            while (!st.isEmpty() &&
+                    st.peek() > c &&
+                    count[st.peek() - 'a'] > 0) {
+
+                visited[st.pop() - 'a'] = false;
+            }
+
+            st.push(c);
+            visited[c - 'a'] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : st)
+            sb.append(c);
+
+        return sb.toString();
+    }
+}
